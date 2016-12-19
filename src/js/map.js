f69ce7a682468e87134a1b18bfdfbf6069358b34
Denis Novikov (dehimer) <den-vse@yandex.ru>
2016-12-19 19:25:09 +0300

    small fixes

--- src/js/map.js
+++ src/js/map.js
@@ -5,7 +5,7 @@ var moscowPosition = [55.7522200, 37.6155600];
 function init(){     
     myMap = new ymaps.Map ("map", {
         center: [moscowPosition[0]+0.002, moscowPosition[1]+0.004],
-        zoom: 15
+        zoom: 16
     });
 
     // var myPlacemark = new ymaps.Placemark([55.8, 37.6]);
